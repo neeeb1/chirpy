d5c66844cfee2d842f5f9494339080f2cf3df837
neeeb1 <18238012+neeeb1@users.noreply.github.com>
2025-09-30 22:04:34 -0700

    Add endpoints for metrics, add middleware for counting metrics

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	serverHits atomic.Int32
+}
+
 func main() {
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", HandlerHealth)
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("GET /api/healthz", HandlerHealth)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.HandlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.HandlerResetMetrics)
+	mux.Handle("/app/", apiCfg.middlewareMetricsIncr(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
 	server := http.Server{}
 
@@ -25,4 +34,36 @@ func HandlerHealth(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	w.Write([]byte("status: OK!"))
+}
+
+func (cfg *apiConfig) middlewareMetricsIncr(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.serverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) HandlerMetrics(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	body := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.serverHits.Load())
+
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) HandlerResetMetrics(w http.ResponseWriter, req *http.Request) {
+	cfg.serverHits.Swap(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	body := fmt.Sprintf("Hits reset to %d", cfg.serverHits.Load())
+
+	w.Write([]byte(body))
 }
