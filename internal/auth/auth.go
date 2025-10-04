1aa2ea43ecd0ed20ed8e354931aa82f056c383f7
neeeb1 <18238012+neeeb1@users.noreply.github.com>
2025-10-03 23:45:45 -0700

    Add jwt creation, validation, and unit tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -10,4 +16,42 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) (bool, error) {
 	match, err := argon2id.ComparePasswordAndHash(password, hash)
 	return match, err
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	str, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return str, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	var userID uuid.UUID
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return userID, err
+	}
+
+	user, err := token.Claims.GetSubject()
+	if err != nil {
+		return userID, err
+	}
+
+	userID, err = uuid.Parse(user)
+	if err != nil {
+		return userID, err
+	}
+
+	return userID, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,12 @@
 package auth
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func TestHashing(t *testing.T) {
 	cases := []struct {
@@ -29,5 +35,90 @@ func TestHashing(t *testing.T) {
 		if actual != c.expected {
 			t.Errorf("failed to check hash: expected %v, got %v", c.expected, actual)
 		}
+	}
+}
+
+func TestJWT(t *testing.T) {
+	type result struct {
+		UserID uuid.UUID
+		err    error
+	}
+
+	type input struct {
+		userID       uuid.UUID
+		tokenSecret  string
+		expiresIn    time.Duration
+		signedSecret string
+	}
+
+	cases := []struct {
+		input    input
+		expected result
+	}{
+		{
+			input: input{
+				userID:       uuid.MustParse("65a8aa5c-b0b4-4240-a6dd-d464bf0a09e7"),
+				tokenSecret:  "myspecialsecret",
+				expiresIn:    time.Minute * 10,
+				signedSecret: "myspecialsecret",
+			},
+			expected: result{
+				UserID: uuid.MustParse("65a8aa5c-b0b4-4240-a6dd-d464bf0a09e7"),
+				err:    nil,
+			},
+		},
+		{
+			input: input{
+				userID:       uuid.MustParse("bf719333-efb5-47c8-9d85-ffb4b6e73996"),
+				tokenSecret:  "myspecialsecret",
+				expiresIn:    time.Millisecond * 10,
+				signedSecret: "myspecialsecret",
+			},
+			expected: result{
+				UserID: uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+				err:    fmt.Errorf("token has invalid claims: token is expired"),
+			},
+		},
+		{
+			input: input{
+				userID:       uuid.MustParse("bf719333-efb5-47c8-9d85-ffb4b6e73996"),
+				tokenSecret:  "myspecialsecret",
+				expiresIn:    time.Minute * 10,
+				signedSecret: "wrongsecret!",
+			},
+			expected: result{
+				UserID: uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+				err:    fmt.Errorf("token signature is invalid: signature is invalid"),
+			},
+		},
+	}
+
+	for _, c := range cases {
+		token, err := MakeJWT(c.input.userID, c.input.tokenSecret, c.input.expiresIn)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+
+		time.Sleep(time.Millisecond * 100)
+
+		var actual result
+		actual.UserID, actual.err = ValidateJWT(token, c.input.signedSecret)
+
+		if c.expected.err == nil && actual.err != nil {
+			t.Errorf("failed: expected %v, got %v", c.expected.err, actual.err)
+		}
+		if c.expected.err != nil && actual.err == nil {
+			t.Errorf("failed: expected %v, got %v", c.expected.err, actual.err)
+		}
+		if c.expected.err != nil && actual.err != nil {
+			if c.expected.err.Error() != actual.err.Error() {
+				t.Errorf("failed: expected %v, got %v", c.expected.err.Error(), actual.err.Error())
+			}
+		}
+
+		if actual.UserID != c.expected.UserID {
+			t.Errorf("failed to validate token: expected %v, got %v", c.expected.UserID, actual.UserID)
+		}
+
 	}
 }
